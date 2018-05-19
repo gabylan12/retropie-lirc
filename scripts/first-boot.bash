b57a70cc11ff50e86abaad501a728347717fad64
gabylan12 <gabriel.radikal@gmail.com>
2018-05-19 19:51:36 -0300

    lirc approved

--- scripts/first-boot.bash
+++ scripts/first-boot.bash
@@ -4,4 +4,5 @@ apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold
 
 echo 'dtoverlay=lirc-rpi,gpio_out_pin=22' | sudo tee --append /boot/config.txt
 sed -i -e 's/\#listen         \= \[address\:\]port/listen \= 0.0.0.0\:8765/g' /etc/lirc/lirc_options.conf
+sed -i "s/devinput/default/" lirc_options.options
 rm first-boot.bash
